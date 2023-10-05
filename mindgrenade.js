cca925f5ab390ce2d08ba774c8cf620044f0fa0b
Harman09 <harmandehal09@gmail.com>
2023-10-05 06:02:35 +0530

    first commit

---
+++ mindgrenade.js
@@ -0,0 +1,7 @@
+let a = 10;
+let b = 5;
+function addvalues() {
+  console.log(`the sum is ${a + b}`);
+}
+
+addvalues();
---
+++ modules.js
@@ -0,0 +1,6 @@
+const names = require("./names");
+const sayHi = require("./utils");
+const sum = require("./mindgrenade");
+//sayHi(secret);
+sayHi(names.john);
+sayHi(names.peter);
